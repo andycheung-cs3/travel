86c30a1d46a01cea69724f129f1a142a7373b128
andy cheung <andycheung.cs3@gmail.com>
2026-03-19 17:10:43 +0800

    fix date

---
+++ js/script2.js
@@ -0,0 +1,10 @@
+
+document.addEventListener("DOMContentLoaded",()=>{
+
+
+
+// toggleButton.addEventListener("click", ()=> {
+//     mobileMenu.classList.toggle("active");
+// });
+const date = (document.getElementById("date").innerHTML=new Date().getFullYear());
+});
